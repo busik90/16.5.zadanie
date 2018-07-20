54ace4386df4e8bf9383e4b33e813c31fcdb1d50
Pawel Bus <pawelbus.dev@outlook.com>
2018-07-20 12:39:50 +0200

    Add scripts which transmit environment to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
 
 //webpack.config.js
-module.exports = {
+module.exports = (env) => {
+  const environment = env || 'production';
+
+  return {
+    mode: environment,
     entry: './src/scripts/app.js',
     output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
+      path: path.resolve(__dirname, 'build'),
+      filename: 'app.' + environment  + '.js'
     }
+  }
 };
